cff9e37df401b504d1b9b3635c80e6f708573c26
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-06-05 11:06:07 +0530

    feat: Navbar completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="relative w-full bg-white text-black">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8 border-b-2">
+        <Link
+          href={"/"}
+          className="inline-flex items-center space-x-2 cursor-pointer"
+        >
+          <span>
+            <Image src="/logo.png" alt="logo" height={50} width={50} />
+          </span>
+          <span className="font-bold">restorePhotos</span>
+        </Link>
+        <div className="flex gap-2 md:gap-5 text-md md:text-xl font-semibold">
+          <Link href={"/"}>Home</Link>|<Link href={"/restore"}>Restore</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
