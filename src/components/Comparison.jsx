b91d89da9505448168020ed5bc8ed769efda9df1
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-06-05 23:16:28 +0530

    feat: comparison component completed

---
+++ src/components/Comparison.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+
+const Comparison = () => {
+  return (
+    <>
+      <section className="px-2 py-10 md:px-0 flex justify-center items-center mx-auto">
+        <div className="mx-auto p-5 rounded-2xl flex md:gap-10 gap-5 flex-col md:flex-row">
+          <div>
+            <h1 className="text-xl font-semibold text-center mb-3">
+              Original Photo
+            </h1>
+            <Image
+              src={"/old.png"}
+              alt="Old Rock"
+              width={500}
+              height={500}
+              className="rounded-3xl"
+            />
+          </div>
+          <div>
+            <h1 className="text-xl font-semibold text-center mb-3">
+              Restored Photo
+            </h1>
+            <Image
+              src={"/new.png"}
+              alt="New Rock"
+              width={500}
+              height={500}
+              className="rounded-3xl"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Comparison;
--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Testimonials from "@/components/Testimonials";
+import Comparison from "@/components/Comparison";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <Comparison />
       <Testimonials />
     </>
   );
