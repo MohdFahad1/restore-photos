4ebc864914444b07d3bb67fbc1f838fbf130a862
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-06-05 22:43:34 +0530

    feat: Testimonials completed

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function Testimonials() {
+  return (
+    <section className="px-2 py-10 md:px-0">
+      <figure className="mx-auto max-w-2xl border-4 border-black p-5 rounded-2xl">
+        <h1 className="mb-4 text-4xl font-semibold text-black">
+          What people are saying ?
+        </h1>
+        <blockquote className="mt-10 text-xl text-gray-900">
+          <p>
+            “restore.io makes photo restoration a breeze. The intuitive
+            step-by-step process is perfect for users of all skill levels.
+            Upload your photos, sit back, and watch as they're transformed into
+            stunning, like-new images in no time. Highly recommended for anyone
+            wanting to rejuvenate their cherished memories!”
+          </p>
+        </blockquote>
+        <figcaption className="mt-10 flex items-center gap-x-6">
+          <div className="isolate flex -space-x-2">
+            <img
+              className="relative z-10 inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              src="https://leerob.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.b1d1472f.jpg&w=256&q=75"
+              alt="Lee_Robinson"
+            />
+            <img
+              className="relative z-0 inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              src="https://nextjs.org/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F35255%2F1665059775-delba.jpg&w=640&q=75"
+              alt="Delba"
+            />
+            <img
+              className="relative z-10 inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              src="https://leerob.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.b1d1472f.jpg&w=256&q=75"
+              alt="Lee_Robinson"
+            />
+            <img
+              className="relative z-0 inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              src="https://nextjs.org/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F35255%2F1665059775-delba.jpg&w=640&q=75"
+              alt="Delba"
+            />
+          </div>
+          <div>
+            <p className="font-semibold text-black">200+ customers</p>
+          </div>
+        </figcaption>
+      </figure>
+    </section>
+  );
+}
--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Testimonials from "@/components/Testimonials";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <Testimonials />
     </>
   );
 };
