5645e4ca8a8ba73d8dd0521db90960ae3ccdde8e
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-06-06 16:16:17 +0530

    feat: added cloudinary upload widget

--- src/app/restore/page.jsx
+++ src/app/restore/page.jsx
@@ -1,7 +1,23 @@
+import Input from "@/components/MediaUploader";
+import Transformed from "@/components/Transformed";
 import React from "react";
 
 const RestorePage = () => {
-  return <div>RestorePage</div>;
+  return (
+    <>
+      <div className="px-2 py-10 md:px-0 flex justify-center items-center mx-auto">
+        <div className="mx-auto p-5 rounded-2xl flex md:gap-10 gap-5 flex-col">
+          <h1 className="md:text-5xl text-3xl font-bold text-center">
+            Restore any photo
+          </h1>
+          <div className="flex flex-col md:flex-row md:gap-20 gap-5">
+            <Input />
+            <Transformed />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RestorePage;
---
+++ src/components/Transformed.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Transformed = () => {
+  return (
+    <div className="mt-5">
+      <h1 className="text-2xl font-bold">Transformed</h1>
+      <div className="flex flex-col gap-4 justify-center items-center bg-gray-200 mt-5 rounded-xl h-[400px] w-[300px]">
+        <p>Transformed Image</p>
+      </div>
+    </div>
+  );
+};
+
+export default Transformed;
