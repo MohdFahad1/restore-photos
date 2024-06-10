104bb00d1a1015f555ad76cb9cc3a5e860680b9f
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-06-10 15:00:38 +0530

    refactor: db connection logic

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,35 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URI;
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
-
-const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
-
-  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
-
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URL, {
-        dbName: "restoreit",
-        bufferCommands: false,
-      })
-      .then((mongoose) => {
-        return mongoose;
-      });
+export const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MONGODB");
+  } catch (error) {
+    console.log("Error connecting to database: ", error);
   }
-
-  cached.conn = await cached.promise;
-
-  return cached.conn;
 };
-
-module.exports = { connectToDatabase };
